3f996c9ecd9d21cdfe923b67f5f2334a2aa091b6
Michael Fulton <mike@michaelfulton.co>
2019-01-24 11:50:04 -0800

    Extracted InputManager into its own class so GameEngine is a little lighter, fixed an issue with incorrect path for background.jpg

--- GameEngine.js
+++ GameEngine.js
@@ -1,4 +1,5 @@
 import Timer from './Timer.js'
+import InputManager from './InputManager.js'
 
 export default class GameEngine {
     constructor() {
@@ -16,6 +17,7 @@ export default class GameEngine {
             function(/* function */ callback, /* DOMElement */ element) {
                 window.setTimeout(callback, 1000 / 60)
             }
+        this.input = new InputManager()
     }
 
     init(ctx) {
@@ -39,125 +41,7 @@ export default class GameEngine {
 
     startInput() {
         console.log('Starting input')
-
-        var getXandY = function(e) {
-            var x = e.clientX - that.ctx.canvas.getBoundingClientRect().left
-            var y = e.clientY - that.ctx.canvas.getBoundingClientRect().top
-
-            if (x < 1024) {
-                x = Math.floor(x / 32)
-                y = Math.floor(y / 32)
-            }
-
-            return { x: x, y: y }
-        }
-
-        var that = this
-
-        // event listeners are added here
-
-        this.ctx.canvas.addEventListener(
-            'click',
-            function(e) {
-                that.click = getXandY(e)
-                console.log(e)
-                console.log(
-                    'Left Click Event - X,Y ' + e.clientX + ', ' + e.clientY
-                )
-            },
-            false
-        )
-
-        this.ctx.canvas.addEventListener(
-            'contextmenu',
-            function(e) {
-                that.click = getXandY(e)
-                console.log(e)
-                console.log(
-                    'Right Click Event - X,Y ' + e.clientX + ', ' + e.clientY
-                )
-                e.preventDefault()
-            },
-            false
-        )
-
-        this.ctx.canvas.addEventListener(
-            'mousemove',
-            function(e) {
-                //console.log(e);
-                that.mouse = getXandY(e)
-            },
-            false
-        )
-
-        this.ctx.canvas.addEventListener(
-            'mousewheel',
-            function(e) {
-                console.log(e)
-                that.wheel = e
-                console.log(
-                    'Click Event - X,Y ' +
-                        e.clientX +
-                        ', ' +
-                        e.clientY +
-                        ' Delta ' +
-                        e.deltaY
-                )
-            },
-            false
-        )
-
-        this.ctx.canvas.addEventListener(
-            'keydown',
-            function(e) {
-                if (
-                    e.code == 'ArrowLeft' ||
-                    e.code == 'ArrowRight' ||
-                    e.code == 'ArrowUp' ||
-                    e.code == 'ArrowDown'
-                ) {
-                    that.playerDirection = e.code
-                    that.playerMoving = true
-                }
-                console.log(e)
-                console.log(
-                    'Key Down Event - Char ' + e.code + ' Code ' + e.keyCode
-                )
-            },
-            false
-        )
-
-        this.ctx.canvas.addEventListener(
-            'keypress',
-            function(e) {
-                if (e.code === 'KeyD') that.d = true
-                that.chars[e.code] = true
-                console.log(e)
-                console.log(
-                    'Key Pressed Event - Char ' +
-                        e.charCode +
-                        ' Code ' +
-                        e.keyCode
-                )
-            },
-            false
-        )
-
-        this.ctx.canvas.addEventListener(
-            'keyup',
-            function(e) {
-                if (e.code == that.playerDirection) {
-                    that.playerMoving = false
-                }
-                console.log(e)
-                console.log(
-                    'Key Up Event - Char ' + e.code + ' Code ' + e.keyCode
-                )
-            },
-            false
-        )
-
-        console.log('Input started')
+        this.input.registerEventListeners(this.ctx)
     }
 
     addEntity(entity) {
---
+++ InputManager.js
@@ -0,0 +1,64 @@
+export default class InputManager {
+    constructor() {
+        this.ctx = null
+        this.downKeys = {}
+        // (x, y) coordinate: Current mouse position over the canvas
+        this.mousePosition = null
+        // (x, y) coordinate: Right clicks, left clicks, all overwrite this value
+        this.lastMouseClickPosition = null
+        // (e) Event object: Last mouse wheel event stored here
+        this.mouseWheel = null
+    }
+
+    registerEventListeners(ctx) {
+        this.ctx = ctx
+
+        this.ctx.canvas.addEventListener('click',
+            e => { this.lastMouseClickPosition = this.getXandY(e) },
+            false
+        )
+
+        this.ctx.canvas.addEventListener('contextmenu',
+            e => {
+                this.lastMouseClickPosition = this.getXandY(e)
+                e.preventDefault()
+            },
+            false
+        )
+
+        this.ctx.canvas.addEventListener('mousemove',
+            e => { this.mousePosition = this.getXandY(e) },
+            false
+        )
+
+        this.ctx.canvas.addEventListener('mousewheel',
+            e => { this.mouseWheel = e }, false)
+
+        this.ctx.canvas.addEventListener('keydown',
+            e => {
+                /** e.code cooresponds to strings describing the key like ArrowUp, ArrowDown, KeyE, KeyW, Digit5, e.t.c */
+                this.downKeys[e.code] = true
+            },
+            false
+        )
+
+        this.ctx.canvas.addEventListener('keyup',
+            e => { this.downKeys[e.code] = null },
+            false
+        )
+
+        console.log('Input started')
+    }
+
+    getXandY(e) {
+        var x = e.clientX - this.ctx.canvas.getBoundingClientRect().left
+        var y = e.clientY - this.ctx.canvas.getBoundingClientRect().top
+
+        if (x < 1024) {
+            x = Math.floor(x / 32)
+            y = Math.floor(y / 32)
+        }
+
+        return { x: x, y: y }
+    }
+}
