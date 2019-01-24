8b4d3b6c619efbafc30b1f60cb8cf19d29552c6d
Michael Fulton <mike@michaelfulton.co>
2019-01-24 15:36:59 -0800

    Rebased to current master

---
+++ Camera.js
@@ -0,0 +1,25 @@
+import Entity from './Entity.js'
+
+export default class Camera extends Entity {
+    constructor(game) {
+        super(game, 0, 0)
+    }
+
+    setFollowedEntity(followedEntity) {
+        this.followedEntity = followedEntity
+    }
+
+    follow() {
+        this.xView = this.followedEntity.x - this.game.ctx.canvas.width / 2
+        this.yView = this.followedEntity.y - this.game.ctx.canvas.height / 2
+    }
+    update() {
+        if (this.followedEntity) {
+            this.follow(this.followedEntity)
+        }
+    }
+
+    draw() {
+        //Empty
+    }
+}
--- main.js
+++ main.js
@@ -1,31 +1,40 @@
 import AssetManager from './AssetManager.js'
-import Background from './Background.js'
 import GameEngine from './GameEngine.js'
-import PlayerCharacter from './PlayerCharacter.js'
+import PlayableCharacter from './PlayerCharacter.js'
+import Map from './Map.js'
+import Camera from './Camera.js'
 
-var AM = new AssetManager()
+let AM = new AssetManager()
 
 AM.queueDownload('./img/RobotUnicorn.png')
 AM.queueDownload('./img/mikeschar.png')
 AM.queueDownload('./img/mushroomdude.png')
 AM.queueDownload('./img/runningcat.png')
-AM.queueDownload('/img/background.jpg')
+AM.queueDownload('./img/background.jpg')
+AM.queueDownload('./img/DungeonColor3@64x64.png')
 
-AM.downloadAll(function () {
+AM.downloadAll(function() {
     var canvas = document.getElementById('gameWorld')
     var ctx = canvas.getContext('2d')
-
     var gameEngine = new GameEngine()
-    gameEngine.init(ctx)
+    let camera = new Camera(gameEngine)
+    gameEngine.init(ctx, gameEngine.camera)
     gameEngine.start()
 
-    gameEngine.addEntity(
-        new Background(gameEngine, AM.getAsset('/img/background.jpg'))
-    )
+    // gameEngine.addEntity(
+    //     new Background(gameEngine, AM.getAsset('./img/background.jpg'))
+    // )
 
     gameEngine.addEntity(
-        new PlayerCharacter(gameEngine, AM.getAsset('./img/mikeschar.png'))
+        new Map(gameEngine, AM.getAsset('./img/DungeonColor3@64x64.png'))
     )
+
+    var player = new PlayableCharacter(gameEngine, AM.getAsset('./img/mikeschar.png'))
+    camera.setFollowedEntity(player)
+    gameEngine.addEntity(player)
+
+    gameEngine.addEntity(camera)
+    gameEngine.camera = camera
 
     console.log('All Done!')
 })
