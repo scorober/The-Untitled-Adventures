402662fa7bd1c8d37a049a0835fd580b83bdcd41
Tanner Brown <tannersbrown@gmail.com>
2019-01-26 19:08:14 -0800

    Updated imports and moved the logic in the callback to the proper scene

--- main.js
+++ main.js
@@ -1,35 +1,41 @@
-import AssetManager from './AssetManager.js'
-import GameEngine from './GameEngine.js'
-import PlayerCharacter from './PlayerCharacter.js'
-import Map from './Map.js'
-import Camera from './Camera.js'
+import AssetManager from './src/utils/AssetManager.js'
+import GameEngine from './src/GameEngine.js'
+// import PlayerCharacter from './src/entities/PlayerCharacter.js'
+// import Map from './src/world/Map.js'
+// import Camera from './src/world/Camera.js'
 
 let AM = new AssetManager()
 
-AM.queueDownload('./img/RobotUnicorn.png')
-AM.queueDownload('./img/mikeschar.png')
-AM.queueDownload('./img/mushroomdude.png')
-AM.queueDownload('./img/runningcat.png')
-AM.queueDownload('./img/background.jpg')
-AM.queueDownload('./img/DungeonColor3@64x64.png')
+AM.queueDownload('./assets/img/RobotUnicorn.png')
+AM.queueDownload('./assets/img/mikeschar.png')
+AM.queueDownload('./assets/img/mushroomdude.png')
+AM.queueDownload('./assets/img/runningcat.png')
+AM.queueDownload('./assets/img/background.jpg')
+AM.queueDownload('./assets/img/DungeonColor3@64x64.png')
 
 AM.downloadAll(function() {
+
     var canvas = document.getElementById('gameWorld')
     var ctx = canvas.getContext('2d')
     var gameEngine = new GameEngine()
-    let camera = new Camera(gameEngine)
+    gameEngine.ASSET_MANAGER = AM
+
+    //let camera = new Camera(gameEngine) //TODO: define this by scene? Not all scenes will need this and it will probably change between scenes.
     gameEngine.init(ctx)
     gameEngine.start()
 
-    gameEngine.addEntity(
-        new Map(gameEngine, AM.getAsset('./img/DungeonColor3@64x64.png'))
-    )
+    // ALL CODE THAT WAS HERE HAS BEEN ADDED TO firstlevel_scene.js
 
-    var player = new PlayerCharacter(gameEngine, AM.getAsset('./img/mikeschar.png'))
-    gameEngine.setCamera(camera)
-    gameEngine.camera.setFollowedEntity(player)
-    gameEngine.addEntity(player)
-    gameEngine.addEntity(camera)
+
+    //gameEngine.addEntity(
+    //    new Map(gameEngine, AM.getAsset('./img/DungeonColor3@64x64.png'))
+    //)
+
+    //var player = new PlayerCharacter(gameEngine, AM.getAsset('./img/mikeschar.png'))
+    //gameEngine.setCamera(camera)
+    //gameEngine.camera.setFollowedEntity(player)
+    //gameEngine.addEntity(player)
+    //gameEngine.addEntity(camera)
 
     console.log('All Done!')
 })
