74042c037c3e62346745cc039034f039969143a9
Tanner Brown <tannersbrown@gmail.com>
2019-01-26 19:05:17 -0800

    Created the first level scene.
    Added entities and logic that originally existed in the AM callback func.
    
    The tilesheet array that was originally in level.js
     has been added to the bottom of the file so a map can be created

---
+++ src/world/scenes/firstlevel_scene.js
@@ -0,0 +1,62 @@
+import Scene from './scene.js'
+import Map from '../Map.js'
+import PlayerCharacter from '../../entities/PlayerCharacter.js'
+
+export default class FirstLevel extends Scene{
+
+    constructor(game){
+        super(game)
+        this.name = 'level1'
+
+
+        var player = new PlayerCharacter(game, game.getAsset('./assets/img/mikeschar.png'))
+        game.CAMERA.setFollowedEntity(player)
+
+
+
+        this.setMap(new Map(game, game.getAsset('./assets/img/DungeonColor3@64x64.png'), 20, 20, 64, 16, TILES))
+
+        this.addEntity(player)
+        this.addEntity(game.CAMERA)
+    }
+
+    /**
+     * Updates this scene.
+     *
+     * @param tick
+     */
+    update(tick) {
+        //NOTE: These two functions were originally done automatically in the super class, but I added them
+        //here to reduce confusion, and to allow the order they are updated/rendered to be adjusted.
+        this.updateMap(tick)
+        this.updateEntities(tick)
+    }
+
+    draw(ctx){
+        this.drawMap(ctx)
+        this.drawEntities(ctx)
+    }
+
+}
+
+var TILES = [7, 7, 7, 5, 5, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 5, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 10, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 12, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 34, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
