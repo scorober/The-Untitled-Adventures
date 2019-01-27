be7a4ce9497a81025a1de48d5e3f51fe46c369ef
Tanner Brown <tannersbrown@gmail.com>
2019-01-26 19:09:22 -0800

    fixed draw function so it gets the ctx as a parameter (like all others)

--- src/entities/Entity.js
+++ src/entities/Entity.js
@@ -6,15 +6,15 @@ export default class Entity {
         this.removeFromWorld = false
     }
 
-    update() { }
+    update(tick) { } // eslint-disable-line no-unused-vars
 
-    draw() {
+    draw(ctx) {
         if (this.game.showOutlines && this.radius) {
-            this.game.ctx.beginPath()
-            this.game.ctx.strokeStyle = 'green'
-            this.game.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-            this.game.ctx.stroke()
-            this.game.ctx.closePath()
+            ctx.beginPath()
+            ctx.strokeStyle = 'green'
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+            ctx.stroke()
+            ctx.closePath()
         }
     }
 
