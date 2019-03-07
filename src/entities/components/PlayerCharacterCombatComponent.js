9fe30d776dd297ae24f6dee421cf172d1fff0e6c
evi <evigold@uw.edu>
2019-03-07 15:40:29 -0800

    fixed scoring duration display, always to 2 decimal points. added player health regeneration

--- src/entities/components/PlayerCharacterCombatComponent.js
+++ src/entities/components/PlayerCharacterCombatComponent.js
@@ -2,12 +2,29 @@ import AnimationComponent from './AnimationComponent.js'
 import { ANIMATIONS as ANIMS } from '../../utils/Const.js'
 import CombatComponent from './CombatComponent.js'
 import MovementComponent from './MovementComponent.js'
+import AttributeComponent from './AttributeComponent.js'
 
 export default class PlayerCharacterCombatComponent extends CombatComponent {
     constructor(entity) {
         super(entity)
     }
 
+    /**
+     * Regenerates players health
+     */
+    update() {
+        super.update()
+        const hp = this.entity.getComponent(AttributeComponent).HP
+        const hpFull = this.entity.getComponent(AttributeComponent).HPFull
+        if(hp > 0 && hp < hpFull) {
+            const temp = (1/(hp + 1)* 0.1)
+            if((hp + temp) > hpFull){
+                this.entity.getComponent(AttributeComponent).HP = hpFull
+            } else {
+                this.entity.getComponent(AttributeComponent).HP += temp
+            }
+        }
+    }
     /**
      * Sets the cooresponding attack animation for PlayerCharacter
      */
